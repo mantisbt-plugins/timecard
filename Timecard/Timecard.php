d4c23409d7120b216cbf5698f29b9319077259b3
John Reese <jreese@leetcode.net>
2009-01-15 15:28:55 -0500

    First pass at form display and processing.

--- Timecard/Timecard.php
+++ Timecard/Timecard.php
@@ -54,11 +54,126 @@ class TimecardPlugin extends MantisPlugin {
 	 */
 	function hooks() {
 		return array(
+			'EVENT_REPORT_BUG_FORM' => 'report_bug_form',
+			'EVENT_REPORT_BUG' => 'report_bug',
+			'EVENT_UPDATE_BUG_FORM' => 'update_bug_form',
+			'EVENT_UPDATE_BUG' => 'update_bug',
+			'EVENT_VIEW_BUG_DETAILS' => 'view_bug',
+
 			'EVENT_MANAGE_PROJECT_CREATE_FORM' => 'project_create_form',
 			'EVENT_MANAGE_PROJECT_CREATE' => 'project_update',
 			'EVENT_MANAGE_PROJECT_UPDATE_FORM' => 'project_update_form',
 			'EVENT_MANAGE_PROJECT_UPDATE' => 'project_update',
 		);
+	}
+
+	/**
+	 * When reporting a bug, show appropriate form elements to the user.
+	 * @param string Event name
+	 * @param int Project ID
+	 * @param boolean Advanced view
+	 */
+	function report_bug_form( $p_event, $p_project_id, $p_advanced ) {
+		if ( plugin_config_get( 'use_timecard' ) ) {
+			$t_project = TimecardProject::load( $p_project_id );
+
+			echo '<tr ', helper_alternate_class(), '><td class="category">', plugin_lang_get( 'timecard' ),
+				'</td><td><input name="plugin_timecard" value="', $t_project->timecard, '" size="15" maxlength="64"/></td></tr>';
+		}
+
+		echo '<tr ', helper_alternate_class(), '><td class="category">', plugin_lang_get( 'estimate' ),
+			'</td><td><input name="plugin_timecard_estimate" value="0" size="8" maxlength="64"/>',
+			plugin_lang_get( 'estimate_hours' ), '</td></tr>';
+	}
+
+	/**
+	 * Process form information when a bug is reported.
+	 * @param string Event name
+	 * @param object Bug Data
+	 * @param int Bug ID
+	 */
+	function report_bug( $p_event, $p_data, $p_bug_id ) {
+		$t_bug = new TimecardBug( $p_bug_id );
+		$t_bug->estimate = gpc_get_int( 'plugin_timecard_estimate', 0 );
+
+		if ( plugin_config_get( 'use_timecard' ) ) {
+			$t_bug->timecard = gpc_get_string( 'plugin_timecard', '' );
+		}
+
+		$t_bug->save();
+	}
+
+	/**
+	 * When updating a bug, show appropriate form elements to the user.
+	 * @param string Event name
+	 * @param int Bug ID
+	 * @param boolean Advanced view
+	 */
+	function update_bug_form( $p_event, $p_bug_id, $p_advanced ) {
+		$t_bug = TimecardBug::load( $p_bug_id, true );
+
+		echo '<tr ', helper_alternate_class(), '><td class="category">', plugin_lang_get( 'estimate' ),
+			'</td><td><input name="plugin_timecard_estimate" value="', $t_bug->estimate, '" size="8" maxlength="64"/>',
+			plugin_lang_get( 'estimate_hours' ), '</td>';
+
+		if ( plugin_config_get( 'use_timecard' ) ) {
+			echo '<td class="category">', plugin_lang_get( 'timecard' ),
+				'</td><td><input name="plugin_timecard" value="', $t_bug->timecard,
+				'" size="15" maxlength="64"/></td><td colspan="2"></td>';
+		} else {
+			echo '<td colspan="4"></td>';
+		}
+
+		echo '</tr>';
+	}
+
+	/**
+	 * Process form information when a bug is updated.
+	 * @param string Event name
+	 * @param object Bug data
+	 * @param int Bug ID
+	 */
+	function update_bug( $p_event, $p_data, $p_bug_id ) {
+		$t_bug = TimecardBug::load( $p_bug_id, true );
+		$t_bug->estimate = gpc_get_int( 'plugin_timecard_estimate', 0 );
+
+		if ( plugin_config_get( 'use_timecard' ) ) {
+			$t_bug->timecard = gpc_get_string( 'plugin_timecard', '' );
+		}
+
+		$t_bug->save();
+	}
+
+	/**
+	 * Show timecard and estimate information when viewing bugs.
+	 * @param string Event name
+	 * @param int Bug ID
+	 * @param boolean Advanced view
+	 */
+	function view_bug( $p_event, $p_bug_id, $p_advanced ) {
+		$t_bug = TimecardBug::load( $p_bug_id, true );
+		$t_bug->calculate();
+
+		echo '<tr ', helper_alternate_class(), '><td class="category">', plugin_lang_get( 'estimate' ),
+			'</td><td>';
+
+		if ( $t_bug->estimate > 0 ) {
+			$t_bug->calculate();
+			echo sprintf( plugin_lang_get( 'estimate_display' ), $t_bug->estimate, $t_bug->estimate - $t_bug->spent );
+		} else {
+			echo plugin_lang_get( 'estimate_zero' );
+		}
+
+		echo '</td>';
+
+		if ( plugin_config_get( 'use_timecard' ) ) {
+			echo '<td class="category">', plugin_lang_get( 'timecard' ), '</td><td>',
+				string_display_line( $t_bug->timecard ), '<td colspan="2"></td>';
+		} else {
+			echo '<td colspan="4"></td>';
+		}
+
+		echo '</tr>';
 	}
 
 	/**
